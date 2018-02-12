c530a2263b6a9f4cfab143780c0500a39f27ba16
bhuvanaramachandran <31499345+bhuvanaramachandran@users.noreply.github.com>
2018-02-12 16:20:58 +0530

    Create find.c

---
+++ find.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+void main()
+{
+  int a[10]={1,2,3,4,5,6},n,i;
+  printf("enter the value");
+  scanf("%d",&n);
+  if(a[i]=n&&n<=6)
+  {
+    printf("yes");
+  }
+  else
+  {
+    printf("no");
+  }
+}
