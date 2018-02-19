348e23134765a34e364a7a88416f8a34ed0938e8
bhuvanaramachandran <31499345+bhuvanaramachandran@users.noreply.github.com>
2018-02-19 16:07:45 +0530

    Update 31.c

--- 31.c
+++ 31.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 #include<string.h>
-int main()
+void main()
 {
 	char str[100],count=0;
 	int i;
 	gets(str);
 	for(i=0;str[i]!='\0';i++)
 	{
-		if(str[i]!=' ')
-		{
-			count++;
-		}
-	}
-	printf("%d",count);
-}
+		if(str[i]!=' ';
+		   {
+			   count++;
+		   }
+		   }
+		   printf("%d",count);
+		   }
