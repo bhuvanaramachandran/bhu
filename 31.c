c306366c232689168383de58cd9ade85e3f95eaf
bhuvanaramachandran <31499345+bhuvanaramachandran@users.noreply.github.com>
2018-02-19 16:01:55 +0530

    Create 31.c

---
+++ 31.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+	char str[100],count=0;
+	int i;
+	gets(str);
+	for(i=0;str[i]!='\0';i++)
+	{
+		if(str[i]!=' ')
+		{
+			count++;
+		}
+	}
+	printf("%d",count);
+}
