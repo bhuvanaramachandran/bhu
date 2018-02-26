344ed6640f01ebf25a481ef3e049f47b500fc951
bhuvanaramachandran <31499345+bhuvanaramachandran@users.noreply.github.com>
2018-02-26 16:50:24 +0530

    Create 51.c

---
+++ 51.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+int main()
+{
+	int num,a[50],i=0,j,rem;
+for(i=0;i<=NULL;i++)
+{
+scanf("%d",&a[i]);
+}
+	while(num!=0)
+	{
+		rem=num%10;
+		num=num/10;
+		i++;
+	}
+	printf("\n the number is:");
+	for(j=i-1;j>=0;j--)
+	{
+		printf("%d  ",a[j]);
+	}
+	return 0;
+}
