7e09bbbf7f82ae629c8b13373f4ecd797f411995
bhuvanaramachandran <31499345+bhuvanaramachandran@users.noreply.github.com>
2018-03-16 16:03:45 +0530

    Create 79.c

---
+++ 79.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+	int a,b,c;
+	printf("enter the value of a and b");
+	scanf("%d %d",&a,&b);
+	c=a*b;
+	if((c==a*a)&&(c=b*b))
+	{
+		printf("\n yes");
+	}
+	else
+	{
+		printf("no");
+	}
+	return 0;
+}
